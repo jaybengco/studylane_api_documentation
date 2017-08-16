3542e80ee946e8e45516eb0ca245de10e4e4e5bd
jay bengco <jay.bengco@tooltwist.com>
2017-08-16 15:17:04 +0800

    ADD gitscripts

---
+++ git_scripts/update_master_and_deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DEPLOYMENT_BRANCH=master
+DEVELOPMENT_BRANCH=development
+
+git fetch && 
+git reset --hard && 
+git checkout $DEPLOYMENT_BRANCH && 
+git pull origin $DEPLOYMENT_BRANCH && 
+git checkout $DEVELOPMENT_BRANCH && 
+git pull origin $DEVELOPMENT_BRANCH && 
+git checkout $DEPLOYMENT_BRANCH &&
+git merge $DEVELOPMENT_BRANCH && 
+git push origin $DEPLOYMENT_BRANCH &&
+echo 'DONE MERGE FROM '$DEVELOPMENT_BRANCH' to '$DEPLOYMENT_BRANCH' branch';
